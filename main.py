e80881c75219ce838875e82cb9ca5761612d33e9
taku-sami <takuya062090@gmail.com>
2021-04-28 18:41:40 +0900

    update main

--- main.py
+++ main.py
@@ -4,6 +4,12 @@ import numpy as np
 import pandas as pd
 from PIL import Image
 
+st.title('STREAMLIT SAMPLER')
+"""
+'''python
+st.title('STREAMLIT SAMPLER')
+'''
+"""
 
 st.title('DataFrame')
 
@@ -14,10 +20,18 @@ df = pd.DataFrame({
 
 st.write('DataFrame')
 st.dataframe(df.style.highlight_max(axis=0))
-
+"""
+'''python
+st.dataframe(df.style.highlight_max(axis=0))
+'''
+"""
 st.write('Table')
 st.table(df)
-
+"""
+'''python
+st.table(df)
+'''
+"""
 
 st.title('Graph')
 
@@ -27,12 +41,28 @@ df2 = DataFrame(
 )
 st.write('Line Chart')
 st.line_chart(df2)
+"""
+'''python
+st.line_chart(df)
+'''
+"""
 
 st.write('Area Chart')
 st.area_chart(df2)
+"""
+'''python
+st.area_chart(df)
+'''
+"""
+
 
 st.write('Bar Chart')
 st.bar_chart(df2)
+"""
+'''python
+st.bar_chart(df)
+'''
+"""
 
 df3 = pd.DataFrame(
     np.random.rand(100, 2) / [50, 50] + [35.69, 139.70],
@@ -41,27 +71,59 @@ df3 = pd.DataFrame(
 
 st.write('Map')
 st.map(df3)
+"""
+'''python
+st.map(df)
+'''
+"""
 
 st.title('Layout')
 
 st.write('expander')
 expander = st.beta_expander('Title')
 expander.write('Body')
+"""
+'''python
+st.write('expander')
+expander = st.beta_expander('Title')
+expander.write('Body')
+'''
+"""
 
 st.write('column')
 left_column, center_column, right_column = st.beta_columns(3)
 left_column.write('column1'), center_column.write(
     'column2'), right_column.write('column3')
+"""
+'''python
+left_column, center_column, right_column = st.beta_columns(3)
+left_column.write('column1'), center_column.write(
+    'column2'), right_column.write('column3')
+'''
+"""
 
 st.title('Widget')
 st.write('image')
 img = Image.open('icon.png')
 st.image(img)
+"""
+'''python
+st.write('image')
+img = Image.open('icon.png')
+st.image(img)
+'''
+"""
 
 st.write('check box')
 
 checked = st.checkbox('Check')
 st.write('status:', checked)
+"""
+'''python
+checked = st.checkbox('Check')
+st.write('status:', checked)
+'''
+"""
 
 st.write('select box')
 option = st.selectbox(
@@ -69,16 +131,44 @@ option = st.selectbox(
     list(range(1, 11)),
 )
 'No.', option
+"""
+'''python
+st.write('select box')
+option = st.selectbox(
+    'Select Number',
+    list(range(1, 11)),
+)
+'No.', option
+'''
+"""
 
 text = st.text_input('Input Text')
 'text:', text
+"""
+'''python
+text = st.text_input('Input Text')
+'text:', text
+'''
+"""
 
 st.write('slider')
 slider_value = st.slider('Set Value', 0, 100, 50)
 'Value:', slider_value
+"""
+'''python
+st.write('slider')
+slider_value = st.slider('Set Value', 0, 100, 50)
+'Value:', slider_value
+'''
+"""
 
 st.sidebar.title('Sidebar')
 st.sidebar.write('Sidebar')
+"""
+'''python
+st.line_chart(df)
+'''
+"""
 
 st.title('Markdown')
 """
